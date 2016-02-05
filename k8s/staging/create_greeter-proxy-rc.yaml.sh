e368f195ed200e82c3b8020aa19cb5d74fba64f5
Mike Metral <metral@gmail.com>
2016-02-05 00:45:19 -0800

    feat(k8s): add dev & staging k8s manifests

---
+++ k8s/staging/create_greeter-proxy-rc.yaml.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cat > nginx-rc.yaml << EOF
+ apiVersion: v1
+ kind: ReplicationController
+ metadata:
+   name: nginx
+   labels:
+     name: nginx
+   namespace: staging
+ spec:
+   replicas: 1
+   selector:
+     name: nginx
+     deployment: ${WERCKER_GIT_COMMIT}
+   template:
+     metadata:
+       labels:
+         name: nginx
+         deployment: ${WERCKER_GIT_COMMIT}
+     spec:
+       containers:
+         - name: nginx
+           image: ${TUTUM_REPO}:${WERCKER_GIT_COMMIT}
+           env:
+             - name: ENABLE_SSL
+               value: "true"
+           ports:
+             - name: http
+               containerPort: 80
+             - name: https
+               containerPort: 443
+           volumeMounts:
+             - name: nginx-env
+               mountPath: /etc/nginx-env
+               readOnly: true
+       imagePullSecrets:
+         - name: tutum-registry-config
+       volumes:
+         - name: nginx-env
+           secret:
+             secretName: nginx-env
+EOF

a5ee2b75dd6a985e184d825fd718abc6940581fa
Mike Metral <metral@gmail.com>
2016-02-05 02:52:11 -0800

    feat(k8s): allow to set ssl/tls in nginx-secret

--- k8s/dev/nginx-secret/config-to-secret.sh
+++ k8s/dev/nginx-secret/config-to-secret.sh
@@ -7,8 +7,18 @@ if [ $# -lt $EXPECTEDARGS ]; then
     exit 0
 fi
 
+# create envvar file for nginx.conf based on user input
 SERVER_NAME=$1
 sed -e "s#{{SERVER_NAME}}#${SERVER_NAME}#g" ./nginx-secret-template > nginx-secret
 
+# set nginx.conf envvar file, nginx-secret, as base64 data in nginx-secret.yaml
 BASE64_ENC=$(cat nginx-secret | base64 --wrap=0)
 sed -e "s#{{CONFIG_DATA}}#${BASE64_ENC}#g#g" ./nginx-secret-template.yaml > nginx-secret.yaml
+
+# set ssl.crt as base64 data in nginx-secret.yaml
+BASE64_ENC=$(cat ssl.crt | base64 --wrap=0)
+sed -i "s#{{SSL_CRT}}#${BASE64_ENC}#g#g" ./nginx-secret.yaml
+
+# set ssl.key as base64 data in nginx-secret.yaml
+BASE64_ENC=$(cat ssl.key | base64 --wrap=0)
+sed -i "s#{{SSL_KEY}}#${BASE64_ENC}#g#g" ./nginx-secret.yaml

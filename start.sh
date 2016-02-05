36562c43f549a32ea4d9d0a925f6e328bf8549c1
Mike Metral <metral@gmail.com>
2016-02-04 20:24:04 -0800

    init commit

---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright 2015 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
+# Remove existing configs
+rm /etc/nginx/conf.d/*.conf
+
+# Env says we're using SSL 
+if [ -n "${ENABLE_SSL+1}" ] && [ "${ENABLE_SSL,,}" = "true" ]; then
+  echo "Enabling SSL..."
+  cp /usr/src/proxy_ssl.conf /etc/nginx/conf.d/proxy.conf
+else
+  # No SSL
+  echo "Enabling *Without* SSL..."
+  cp /usr/src/proxy_nossl.conf /etc/nginx/conf.d/proxy.conf
+fi
+
+show_val() { echo "${!1}"; }
+
+cat /etc/nginx/conf.d/proxy.conf
+
+echo "Starting nginx..."
+nginx -g 'daemon off;'

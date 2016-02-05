c727d2b4537c3e3fabc6878e3ef4144a53f0aa04
Mike Metral <metral@gmail.com>
2016-02-05 13:14:17 -0800

    refactor(k8s): use ssl filepaths for nginx secret

--- k8s/dev/nginx-secret/config-to-secret.sh
+++ k8s/dev/nginx-secret/config-to-secret.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
 # Encodes the environment variables into a Kubernetes secret.
-EXPECTEDARGS=1
+EXPECTEDARGS=3
 if [ $# -lt $EXPECTEDARGS ]; then
-  echo "Usage: $0 <SERVER_NAME>"
-    exit 0
+  echo "Usage: $0 <SERVER_NAME> <SSL_CRT_FILEPATH> <SSL_KEY_FILEPATH>"
+    exit 1
 fi
 
-# create envvar file for nginx.conf based on user input
 SERVER_NAME=$1
+SSL_CRT_FILEPATH=$2
+SSL_KEY_FILEPATH=$3
+
+# create nginx-secret (envvar file for nginx.conf) based on user input
 sed -e "s#{{SERVER_NAME}}#${SERVER_NAME}#g" ./nginx-secret-template > nginx-secret
 
-# set nginx.conf envvar file, nginx-secret, as base64 data in nginx-secret.yaml
+# set nginx-secret (envvar file for nginx.conf) contents as
+# base64 data in nginx-secret.yaml
 BASE64_ENC=$(cat nginx-secret | base64 --wrap=0)
 sed -e "s#{{CONFIG_DATA}}#${BASE64_ENC}#g#g" ./nginx-secret-template.yaml > nginx-secret.yaml
 
 # set ssl.crt as base64 data in nginx-secret.yaml
-BASE64_ENC=$(cat ssl.crt | base64 --wrap=0)
+BASE64_ENC=$(cat $SSL_CRT_FILEPATH | base64 --wrap=0)
 sed -i "s#{{SSL_CRT}}#${BASE64_ENC}#g#g" ./nginx-secret.yaml
 
 # set ssl.key as base64 data in nginx-secret.yaml
-BASE64_ENC=$(cat ssl.key | base64 --wrap=0)
+BASE64_ENC=$(cat $SSL_KEY_FILEPATH | base64 --wrap=0)
 sed -i "s#{{SSL_KEY}}#${BASE64_ENC}#g#g" ./nginx-secret.yaml
